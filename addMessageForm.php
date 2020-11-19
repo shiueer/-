938ce58e81a5efed48a628c3e7315dca6db1ef02
學而\eric8 <eric89425@gmail.com>
2020-11-19 16:47:35 +0800

    set apply film that student can't edit

--- addMessageForm.php
+++ addMessageForm.php
@@ -36,35 +36,35 @@ require("dbconnect.php");
       <tr>
         <th>申請補助總類</th>
         <th>
-        <input type="radio" name = "role" value = "0"><label>低收入戶</label>
-        <input type="radio" name = "role" value = "1"><label>中低收入戶</label>
-        <input type="radio" name = "role" value = "2"><label>家庭突發因素</label>
+        <input type="radio" name = "role" value = "0" checked disabled><label>低收入戶</label>
+        <input type="radio" name = "role" value = "1" checked disabled><label>中低收入戶</label>
+        <input type="radio" name = "role" value = "2" checked disabled><label>家庭突發因素</label>
         </th>
         <th>導師訪視說明</th>
         <th>
-        <input name="title" type="text" id="title" /></th>
+        <input name="title" type="text" id="title" readonly /></th>
         </th>
       <tr>
         <th>秘書審核</th>
         <th>審核結果
-          <input type="radio" name = "role" value = "2"><label></label>准於補助
-          <input name="title" type="text" id="title" />元</th>
+          <input type="radio" name = "role" value = "2" checked disabled ><label></label>准於補助
+          <input name="title" type="text" id="title" readonly />元</th>
           <th>導師簽章</th>
-          <th><input type="radio" name = "role" value = "2"><label>同意</label></th>
+          <th><input type="radio" name = "role" value = "2" checked disabled><label>同意</label></th>
         </th>
       </tr>
       </tr>
       <tr>
         <th>秘書審查意見</th>
           <th>
-            <input name="title" type="text" id="title" /></th>
+            <input name="title" type="text" id="title" readonly/></th>
           </th>
           <th>秘書簽章</th>
-          <th><input type="radio" name = "role" value = "2"><label>同意</label></th>
+          <th><input type="radio" name = "role" value = "2" checked disabled><label>同意</label></th>
       </tr>
       <tr>
         <th>校長簽章</th>
-        <th><input type="radio" name = "role" value = "2"><label>同意</label></th>
+        <th><input type="radio" name = "role" value = "2" checked disabled><label>同意</label></th>
         </th>
       </tr>
     </thead> 
