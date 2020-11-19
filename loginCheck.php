e81a1c5f0f00a26394d6ed274c6c41904d1f9571
學而\eric8 <eric89425@gmail.com>
2020-11-19 14:01:30 +0800

    update login check for navigate to their page

--- loginCheck.php
+++ loginCheck.php
@@ -8,11 +8,17 @@ $role = $_POST['role'];
 
 if (checkUserIDPwd($userName,$passWord,$role)) {
 	//provide a link to the message list UI
-	if($role == 1){
-		echo "<a href='listtodo.php'>go</a>";
+	if($role == 0){//role is student
+		echo "<a href='listtodo_student.php'>go</a>";
 	}
-	else{
-		echo "<a href='listtodo_employee.php'>go</a>";
+	if($role == 1){//role is teacher
+		echo "<a href='listtodo_teacher.php'>go</a>";
+	}
+	if($role == 2){//role is secretary
+		echo "<a href='listtodo_secretary.php'>go</a>";
+	}
+	if($role == 3){//role is principal
+		echo "<a href='listtodo_principal.php'>go</a>";
 	}
 } else {
 	//print error message
