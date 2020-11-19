81c2064a9c221b71dbd57ce3ec8762b053f2c996
學而\eric8 <eric89425@gmail.com>
2020-11-19 14:08:32 +0800

    merge pages for different roles

--- listtodo_student.php
+++ listtodo_student.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 session_start();
 require("dbconnect.php");
@@ -34,40 +33,3 @@ while (	$rs=mysqli_fetch_assoc($result)) {
 ?>
 </body>
 </html>
-=======
-<?php
-session_start();
-require("dbconnect.php");
-$sql = "select * from todolist where Status = 0;";
-$result=mysqli_query($conn,$sql) or die("DB Error: Cannot retrieve message.");
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>無標題文件</title>
-</head>
-
-<body>
-<a href="loginForm.php"><button>OUT</button></a>
-<p>my to do list !! </p>
-<hr/>
-<table width="200" border="1">
-  <tr>
-    <td>ID</td>
-    <td>Title</td>
-    <td>Content</td>
-    <td>Status</td>
-  </tr>
-<?php
-while (	$rs=mysqli_fetch_assoc($result)) {
-	echo "<tr><td>" . $rs['ID'] . "</td>";
-	echo "<td>{$rs['Title']}</td>";
-	echo "<td>" , $rs['Content'], "</td>";
-  echo "<td>" . $rs['Status'] ,"</td>";
-  echo "<td><a href= 'listtoset_employee.php?ID={$rs['ID']}'>OK</a>". "</td></tr>";
-}
-?>
-</body>
-</html>
->>>>>>> c818902a5f8336321d9372b63736eceae5e612f4
