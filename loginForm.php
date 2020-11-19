f56402bfd2152eba6a1baa442ccd6fda50cfa08c
學而\eric8 <eric89425@gmail.com>
2020-11-19 13:54:43 +0800

    update the login form for new roles

--- loginForm.php
+++ loginForm.php
@@ -9,8 +9,10 @@ $_SESSION['uID'] = "";
 <form method="post" action="loginCheck.php">
 User Name: <input type="text" name="id"><br />
 Password : <input type="password" name="pwd"><br />
-Role : <input type="radio" name="role" value="0"><label>employee</label>
-<input type="radio" name="role" value="1"><label>boss</label><br/>
+Role : <input type="radio" name="role" value="0"><label>student</label>
+<input type="radio" name="role" value="1"><label>teacher</label>
+<input type="radio" name="role" value="2"><label>secretary</label>
+<input type="radio" name="role" value="3"><label>principal</label><br/>
 <input type="submit">
 </form>
 
