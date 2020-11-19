ee703a46762924369a4c2280c3724a040077f843
學而\eric8 <eric89425@gmail.com>
2020-11-19 16:49:52 +0800

    fix code format

--- loginForm.php
+++ loginForm.php
@@ -5,11 +5,12 @@ require("dbconnect.php");
 //set the login mark to empty
 $_SESSION['uID'] = "";
 ?>
-<h1>Login Form</h1><hr />
+<h1>Login Form</h1><hr/>
 <form method="post" action="loginCheck.php">
-User Name: <input type="text" name="id"><br />
-Password : <input type="password" name="pwd"><br />
-Role : <input type="radio" name="role" value="0"><label>student</label>
+User Name: <input type="text" name="id"><br/>
+Password : <input type="password" name="pwd"><br/>
+Role :
+<input type="radio" name="role" value="0"><label>student</label>
 <input type="radio" name="role" value="1"><label>teacher</label>
 <input type="radio" name="role" value="2"><label>secretary</label>
 <input type="radio" name="role" value="3"><label>principal</label><br/>
